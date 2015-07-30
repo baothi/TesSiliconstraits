31888ad50439db718e210390c5565d6adadfdbff
baothi <baothi246@gmail.com>
2015-07-30 18:10:17 +0700

    Signed-off-by: baothi <baothi246@gmail.com>

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
   belongs_to :user
   has_many :comments
-  def self.name
-    Product.user.profile.name
+  def name
+    try(:user).try(:profile).try(:name).where()
   end
 end
